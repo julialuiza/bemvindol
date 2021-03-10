9c1df379d9d03f2cc508a620beb067b19d282745
JuliaLuiza <jlslc@icomp.ufam.edu.br>
2021-03-10 00:51:02 -0400

    [feature] Configura conexao com MongoDB

---
+++ src/util/dbConnect.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+async function dbConnect() {
+  if (mongoose.connection.readyState >= 1) {
+    return
+  }
+
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+}
+
+export default dbConnect
