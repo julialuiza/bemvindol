b172b6bf064db778d9526c6f7f58e52c974adaf5
JuliaLuiza <jlslc@icomp.ufam.edu.br>
2021-03-10 00:46:53 -0400

    [enhancement] Ajusta arquivos iniciais (index, app, document)

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import LeftSide from '../components/LeftSide';
+import SignUp from '../components/Signup';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Bemvindol | Cadastro </title>
+      </Head>
+      <section>
+        <div>
+          <LeftSide />
+        </div>
+        <div>
+          <SignUp />
+        </div>
+      </section>
+    </div>
+  );
+}
